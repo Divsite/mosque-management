6ef1b54a90a75a89cbb418a21004df0e47c6ce8c
samsul97 <samsulaculhadi@gmail.com>
2023-09-17 14:57:03 +0700

    add dynamic navbar

--- system/frontend/views/layouts/main.php
+++ system/frontend/views/layouts/main.php
@@ -7,11 +7,13 @@ use yii\helpers\Html;
 use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
 use yii\bootstrap4\Breadcrumbs;
+use yii\widgets\Menu;
 //use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use common\widgets\Alert;
 
 AppAsset::register($this);
+$this->title = Yii::t('app', 'mosque_hub'); 
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -28,7 +30,9 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <div class="wrap">
+    
     <?php
+    
     NavBar::begin([
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
@@ -36,28 +40,82 @@ AppAsset::register($this);
             'class' => 'navbar navbar-expand-lg navbar-dark bg-primary navbar-fixed-top',
         ],
     ]);
+
+    $guestMode = Yii::$app->user->isGuest;
+
+    function generateSubMenu($parentId, $userMenus, $guestMode) {
+        $subMenuItems = [];
+        foreach ($userMenus as $userMenu) {
+            if ($userMenu['id_sub'] == $parentId && $userMenu['guest'] == ($guestMode ? 0 : 1)) {
+                $menuLabel = $userMenu['name'];
+                $menuUrl = [$userMenu['url_controller'] . '/' . $userMenu['url_view']];
+                
+                // Query untuk mengambil submenu sesuai dengan menu utama
+                $subMenus = generateSubMenu($userMenu['id'], $userMenus, $guestMode);
+                
+                if (!empty($subMenus)) {
+                    $subMenuItems[] = [
+                        'label' => $menuLabel,
+                        'items' => $subMenus,
+                        'dropdownOptions' => ['class' => 'dropdown-menu'],
+                    ];
+                } else {
+                    $subMenuItems[] = ['label' => $menuLabel, 'url' => $menuUrl];
+                }
+            }
+        }
+        return $subMenuItems;
+    }
+    
+    // get menu data
+    $userMenus = \backend\models\UserMenu::find()
+        ->where(['module' => Yii::$app->controller->module->id, 'id_sub' => 0, 'guest' => ($guestMode ? 0 : 1)])
+        ->orderBy(['seq' => SORT_ASC, 'id' => SORT_DESC])
+        ->asArray()
+        ->all();
+    
+    // general menus 
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
+    
+    foreach ($userMenus as $userMenu) {
+        // sub menu level 1
+        if ($userMenu['id_sub'] == 0) {
+            $menuLabel = $userMenu['name'];
+            $menuUrl = [$userMenu['url_controller'] . '/' . $userMenu['url_view']];
+    
+            $subMenus = generateSubMenu($userMenu['id'], $userMenus, $guestMode);
+    
+            if (!empty($subMenus)) {
+                $menuItems[] = [
+                    'label' => $menuLabel,
+                    'items' => $subMenus,
+                    'dropdownOptions' => ['class' => 'dropdown-menu'],
+                ];
+            } else {
+                $menuItems[] = ['label' => $menuLabel, 'url' => $menuUrl];
+            }
+        }
+    }
+    
+    // all menu from db automatic sign in here 
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
+        $menuItems[] = ['label' => 'Profile', 'url' => ['/user/view', 'id' => Yii::$app->user->identity->id]];
+        $menuItems[] = ['label' => 'Logout', 'url' => ['/site/logout'], 'linkOptions' => ['data-method' => 'post']];
     }
+    
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
+        'options' => ['class' => 'navbar-nav ml-auto'],
         'items' => $menuItems,
+        'encodeLabels' => false,
+        'activateItems' => true,
+        'activateParents' => true,
     ]);
+    
     NavBar::end();
     ?>
 
